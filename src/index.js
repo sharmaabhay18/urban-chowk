b075bc6160616b372407e1e39718041ab28fe8f1
Abhay Sharma <abhay@Abhays-MacBook-Pro.local>
2021-11-10 18:42:44 -0500

    intial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
+import "./index.module.scss";
+import App from "./pages/app";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
+);
