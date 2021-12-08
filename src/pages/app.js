a2857d11ae6a8760434ae60529e9272592282c48
KandasamyParthasarathy <kparthas@stevens.edu>
2021-12-08 17:20:07 -0500

    correcting the index issue

--- src/pages/app.js
+++ src/pages/app.js
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 import Spinner from "components/Spinner";
 
-import Home from "pages/home";
+import Home from "pages/Home";
 import SignUp from "pages/signUp";
 import Login from "pages/login";
 import ForgetPassword from "pages/forgetPassword";
